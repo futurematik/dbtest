ae969aa4dc525af28c86860adc165a4a6263e707
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2019-10-06 14:03:09 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import 'jest';
+import process from 'process';
+import { Pool } from 'pg';
+import nanoid from 'nanoid';
+
+export interface Context {
+  pool: Pool;
+}
+
+export function connectTestDatabase(each = false): Context {
+  const dbUser = process.env.DB_USER || 'postgres';
+  const dbName = process.env.DB_NAME || `test_${nanoid()}`;
+  const context: Partial<Context> = {};
+
+  async function before(): Promise<void> {
+    context.pool = new Pool({ database: 'postgres', user: dbUser });
+    await context.pool.query(`CREATE DATABASE ${dbName}`);
+    await context.pool.end();
+    context.pool = new Pool({ database: dbName, user: dbUser });
+  }
+
+  async function after(): Promise<void> {
+    if (context.pool) {
+      await context.pool.end();
+      context.pool = new Pool({ database: 'postgres', user: dbUser });
+      await context.pool.query(`DROP DATABASE ${dbName}`);
+      await context.pool.end();
+      context.pool = undefined;
+    }
+  }
+
+  if (each) {
+    beforeEach(before);
+    afterEach(after);
+  } else {
+    beforeAll(before);
+    afterAll(after);
+  }
+
+  // this is casted to be nice; whenever a test is running then pool will have
+  // a value
+  return context as Context;
+}
